8829d6bb55fb41411b87258b12c74014ae9bac8c
Steven Serrata <sserrata@paloaltonetworks.com>
2019-12-17 10:24:48 -0600

    Added loading and thankyou messages

--- src/components/form.js
+++ src/components/form.js
@@ -4,14 +4,15 @@ export default class Form extends React.Component {
   constructor(props, context) {
     super(props, context);
     this.state = {
-      formLoaded: false
+      formLoaded: false,
+      formSubmitted: false
     };
   }
 
   componentDidMount() {
     if (!this.state.formLoaded) {
       MktoForms2.loadForm(
-        "https:////app-ab28.marketo.com",
+        "https://app-ab28.marketo.com",
         "531-OCS-018",
         2572,
         () => this.setState({ formLoaded: true })
@@ -19,11 +20,20 @@ export default class Form extends React.Component {
     }
   }
 
+  submitForm(e) {
+    this.setState({ formSubmitted: true });
+  }
+
   render() {
-    return (
-      <>
-        <form id="mktoForm_2572" />
-      </>
-    );
+    if (!this.state.formSubmitted) {
+      return (
+        <>
+          {!this.state.formLoaded && <div>Loading...</div>}
+          <form id="mktoForm_2572" onSubmit={this.submitForm} />
+        </>
+      );
+    } else {
+      return <div>Thank you!!!!</div>;
+    }
   }
 }
