1d4a4d6236af938b80a1673aa1a8847cf3b95439
csestito <csestito@paloaltonetworks.com>
2021-11-09 11:29:34 -0800

    fix links

--- static/oas/expander/sidebar.js
+++ static/oas/expander/sidebar.js
@@ -4,7 +4,13 @@ const fs = require("fs");
 // Use the following to frontload docs
 // Uncomment the following line when we have an intro doc
 // var docs = ["expander/expander"];
-var docs = [];
+var docs = [
+  {
+    type: "link",
+    label: "Expander API",
+    href: "/api/expander/annotations-api",
+  },
+];
 
 // Change these variables to match your doc path
 const relativePath = "expander";
